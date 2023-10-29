496bdcaecc08937e82d0c08f158dd0082d834dd8
vagrant <vagrant@localhost.localdomain>
2023-10-29 07:10:20 +0000

    [Finish] 3-8

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_22_102130) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_29_063310) do
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
